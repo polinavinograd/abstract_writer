b0dd9fb3062936c508d126c43a8de9ed4e948de1
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-11-15 11:38:52 +0300

    server

---
+++ back/main.py
@@ -0,0 +1,5 @@
+def keyword_based_abstract(text: str) -> str:
+    return "le kw abstract"
+
+def machine_learning_abstract(text: str) -> str:
+    return "le ml abstract"
--- back/server.py
+++ back/server.py
@@ -1,14 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
-from document import Document
+from main import create_abstract
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/create_abstract', methods=['POST'])
-def create_abstract():
-    pass
+@app.route('/kwabstract', methods=['POST'])
+def kwabstract():
+    return create_abstract(request.json["text"])
 
+@app.route('/mlabstract', methods=['POST'])
+def mlabstract():
+    return create_abstract(request.json["text"])
 
 if __name__ == '__main__':
     app.run()
