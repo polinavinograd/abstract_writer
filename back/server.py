58586f5ede2ade321d2f20dc17aa0863767814c4
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2023-11-15 11:52:41 +0300

    fix server

--- back/server.py
+++ back/server.py
@@ -1,17 +1,17 @@
 from flask import Flask, request
 from flask_cors import CORS
-from main import create_abstract
+from main import keyword_based_abstract, machine_learning_abstract
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/kwabstract', methods=['POST'])
 def kwabstract():
-    return create_abstract(request.json["text"])
+    return keyword_based_abstract(request.json["text"])
 
 @app.route('/mlabstract', methods=['POST'])
 def mlabstract():
-    return create_abstract(request.json["text"])
+    return machine_learning_abstract(request.json["text"])
 
 if __name__ == '__main__':
     app.run()
